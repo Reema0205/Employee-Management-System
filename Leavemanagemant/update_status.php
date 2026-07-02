b94146633ff4bcebfce9b3d7e3b6d9cc2612bfc2
hasmathashfa <hasmathashfa@gmail.com>
2026-07-02 12:59:04 +0530

    Testing update

---
+++ Leavemanagemant/update_status.php
@@ -0,0 +1,25 @@
+<?php
+
+include "db.php";
+
+$id = $_POST['id'];
+$status = $_POST['status'];
+
+$sql = "UPDATE leaves
+        SET status = ?
+        WHERE id = ?";
+
+$stmt = $conn->prepare($sql);
+
+$stmt->bind_param("si", $status, $id);
+
+if($stmt->execute()){
+    echo "success";
+}else{
+    echo "error";
+}
+
+$stmt->close();
+$conn->close();
+?>
+
